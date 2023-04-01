ac44bf15ca350b2e62173889a4a75a343977718e
Max Tyler Simon <mtsimon@wm.edu>
2023-04-01 10:45:04 -0400

    New dependency for date/time functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 const mainGreen = Color(0xFF165740);
 
@@ -12,119 +13,111 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+
+    String createDate(int day) {
+      DateTime date = DateTime.now();
+      if (day > 0){
+        date = date.add(Duration(days: day));
+      }
+      String dateFormat = DateFormat("EEE, MMM d").format(date);
+      return dateFormat;
+    }
+
     return Scaffold(
 
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Container(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
 
-          child: Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(25.0),
-              ),
-              Align(
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(25.0),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
                 alignment: Alignment.topCenter,
-                child: Container(
-                  alignment: Alignment.topCenter,
-                  margin: EdgeInsets.zero,
-                  padding: EdgeInsets.zero,
-                  width: 500,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xff165740),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.zero,
-                    border: Border.all(color: const Color(0x4d9e9e9e), width: 1),
-                  ),
-                  child: const Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "TableTogether",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 35,
-                        color: Color(0xffffffff),
-                      ),
+                margin: EdgeInsets.zero,
+                padding: EdgeInsets.zero,
+                width: 500,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: const Color(0xff165740),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.zero,
+                  border: Border.all(color: const Color(0x4d9e9e9e), width: 1),
+                ),
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "TableTogether",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 35,
+                      color: Color(0xffffffff),
                     ),
                   ),
                 ),
               ),
+            ),
 
-              const Padding(
-                padding: EdgeInsets.all(25.0),
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+            ),
+
+            const Text(
+              "Choose a Day",
+              textAlign: TextAlign.start,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+                fontSize: 35,
+                color: Color(0xff000000),
               ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+
+            SizedBox(
+              width: 300.0,
+              height: 80.0,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
+                  ),
+                  onPressed: () { },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(createDate(0), style: const TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+            ),
 
               SizedBox(
                 width: 300.0,
                 height: 80.0,
                 child: Padding(
-                  padding: EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                       backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
                     ),
                     onPressed: () { },
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Tuesday April 4", style: TextStyle(fontSize: 20)),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(
-                width: 300.0,
-                height: 80.0,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: TextButton(
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
-                    ),
-                    onPressed: () { },
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Tuesday April 4", style: TextStyle(fontSize: 20)),
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(
-                width: 300.0,
-                height: 80.0,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: TextButton(
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
-                    ),
-                    onPressed: () { },
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Tuesday April 4", style: TextStyle(fontSize: 20)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(createDate(1), style: const TextStyle(fontSize: 20)),
                     ),
                   ),
                 ),
@@ -134,16 +127,16 @@ class _HomePage extends State<HomePage> {
                 width: 300.0,
                 height: 80.0,
                 child: Padding(
-                  padding: EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                       backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
                     ),
                     onPressed: () { },
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Tuesday April 4", style: TextStyle(fontSize: 20))
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(createDate(2), style: const TextStyle(fontSize: 20)),
                     ),
                   ),
                 ),
@@ -153,16 +146,35 @@ class _HomePage extends State<HomePage> {
                 width: 300.0,
                 height: 80.0,
                 child: Padding(
-                  padding: EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                       backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
                     ),
                     onPressed: () { },
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Tuesday April 4", style: TextStyle(fontSize: 20))
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(createDate(3), style: const TextStyle(fontSize: 20))
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                width: 300.0,
+                height: 80.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
+                    ),
+                    onPressed: () { },
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(createDate(4), style: const TextStyle(fontSize: 20))
                     ),
                   ),
                 ),
@@ -173,16 +185,16 @@ class _HomePage extends State<HomePage> {
                 width: 300.0,
                 height: 80.0,
                 child: Padding(
-                  padding: EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                       backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
                     ),
                     onPressed: () { },
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Tuesday April 4", style: TextStyle(fontSize: 20))
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(createDate(5), style: const TextStyle(fontSize: 20))
                     ),
                   ),
                 ),
@@ -192,16 +204,16 @@ class _HomePage extends State<HomePage> {
                 width: 300.0,
                 height: 80.0,
                 child: Padding(
-                  padding: EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                       backgroundColor: MaterialStateProperty.all<Color>(mainGreen),
                     ),
                     onPressed: () { },
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Tuesday April 4", style: TextStyle(fontSize: 20))
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(createDate(6), style: const TextStyle(fontSize: 20))
                     ),
                   ),
                 ),
@@ -211,15 +223,13 @@ class _HomePage extends State<HomePage> {
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.calendar_month), label: 'Calendar'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-
-    );
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.calendar_month), label: 'Calendar'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        )
+      );
   }
 }
